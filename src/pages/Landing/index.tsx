ca44e03b0d486ce17c9aeeab6298390c07f545eb
jasonk16 <kahkeat98@gmail.com>
2023-10-23 16:23:50 +0800

    removed all files

--- src/pages/Landing/index.tsx
+++
@@ -1,9 +0,0 @@
-const Landing = () => {
-  return (
-    <div className="flex h-screen bg-indigo-500 justify-center items-center">
-      <h1 className="text-7xl">Welcome to the website</h1>
-    </div>
-  );
-};
-
-export default Landing;
